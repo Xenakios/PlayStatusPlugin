95338ff659f7c470b3d1dd982595e6626b9580f3
xenakios <xenakios@gmail.com>
2019-03-16 21:08:30 +0200

    Initial git commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+PlayStatusPluginAudioProcessorEditor::PlayStatusPluginAudioProcessorEditor (PlayStatusPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    setSize (400, 100);
+	startTimer(50);
+}
+
+PlayStatusPluginAudioProcessorEditor::~PlayStatusPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void PlayStatusPluginAudioProcessorEditor::paint (Graphics& g)
+{
+	AudioPlayHead::CurrentPositionInfo& info = processor.positionInfo;
+	Colour color{ Colours::black };
+	if (info.isPlaying)
+		color = Colours::green;
+	if (info.isRecording)
+		color = Colours::red;
+	g.fillAll(color);
+}
+
+void PlayStatusPluginAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void PlayStatusPluginAudioProcessorEditor::timerCallback()
+{
+	repaint();
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PlayStatusPluginAudioProcessorEditor  : public AudioProcessorEditor, public Timer
+{
+public:
+    PlayStatusPluginAudioProcessorEditor (PlayStatusPluginAudioProcessor&);
+    ~PlayStatusPluginAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+	void timerCallback() override;
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PlayStatusPluginAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlayStatusPluginAudioProcessorEditor)
+};
